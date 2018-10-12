d17743c877eb54ded19280c11977e2c7dac124b2
Eric Du <16ezd@caslab.queensu.ca>
2018-10-12 16:31:24 -0400

    add agent and planner logout

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
+def login():
+    testinput = input()
+    if testinput == "agent":
+        print("agent clear")
+        agent()
+    elif testinput == "planner":
+        print("planner clear")
+        planner()
+    else:
+        print("wrong")
+
+def agent():
+    loggedIn = True
+    while (loggedIn):
+        print("in agent")
+        testinput = input()
+        if testinput == "logout":
+            loggedIn = False
+
+def planner():
+    loggedIn = True
+    while (loggedIn):
+        print("in planner")
+        testinput = input()
+        if testinput == "logout":
+            loggedIn = False
+
+
 while(True):
     testinput = input()
     if testinput == "login":
-        testinput = input()
-        if testinput == "agent":
-            print("agent clear")
-        elif testinput == "planner":
-            print("planner clear")
-        else:
-            print("wrong")
+        login()
+
