789fb1bae435de588e518a3268375f4bea48aabc
Eric Du <16ezd@caslab.queensu.ca>
2018-10-12 18:26:40 -0400

    read VSF

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ def login():
         print("wrong")
 
 def agent():
+    validServices = []
+    ReadVSF("vsf.txt", validServices)
+    for services in validServices:
+        print(services)
+
     loggedIn = True
     while (loggedIn):
         print("in agent")
@@ -18,6 +23,11 @@ def agent():
             loggedIn = False
 
 def planner():
+    validServices = []
+    ReadVSF("vsf.txt", validServices)
+    for services in validServices:
+        print(services)
+
     loggedIn = True
     while (loggedIn):
         print("in planner")
@@ -30,30 +40,54 @@ def planner():
 def createservice():
     print("now in createservice mode")
     serviceNumber = input()
-    if len(serviceNumber) != 5:
-        print("error: must be length 5")
-    elif serviceNumber[0] == "0":
-        print("error: cannot start with 0")
+    if not ValidServNumber(serviceNumber):
+        print("not valid service number")
     else:
         serviceDate = input()
-        if len(serviceDate) != 8:
-            print("error: must be length 8")
-        elif not isNumeric(serviceDate):
-            print("error: must be numeric")
-        elif int(serviceDate[0:4]) < 1980 or int(serviceDate[0:4]) > 2999:
-            print("error: year out of range")
-        elif int(serviceDate[4:6]) < 1 or int(serviceDate[4:6]) > 13:
-            print("error: month out of range")
-        elif int(serviceDate[6:8]) < 1 or int(serviceDate[6:8]) > 32:
-            print("error: day out of range")
+        if not ValidDate(serviceDate):
+            print("not valid date")
         else:
             serviceName = input()
-            if len(serviceName) < 3 or len(serviceName) > 39:
-                print("error: service name invalid length")
-            elif serviceName[0] == " " or serviceName[-1] == " ":
-                print("error: service name cannot begin or end with space")
+            if not ValidServName(serviceName):
+                print("not valid name")
 
 
+def ValidServNumber(num):
+    if len(num) != 5:
+        print("error: must be length 5")
+        return False
+    elif num[0] == "0":
+        print("error: cannot start with 0")
+        return False
+    else:
+        return True
+
+def ValidServName(name):
+    if len(name) < 3 or len(name) > 39:
+        print("error: service name invalid length")
+    elif name[0] == " " or name[-1] == " ":
+        print("error: service name cannot begin or end with space")
+    else:
+        return True
+
+def ValidDate(date):
+    if len(date) != 8:
+        print("error: must be length 8")
+        return False
+    elif not isNumeric(date):
+        print("error: must be numeric")
+        return False
+    elif int(date[0:4]) < 1980 or int(date[0:4]) > 2999:
+        print("error: year out of range")
+        return False
+    elif int(date[4:6]) < 1 or int(date[4:6]) > 13:
+        print("error: month out of range")
+        return False
+    elif int(date[6:8]) < 1 or int(date[6:8]) > 32:
+        print("error: day out of range")
+        return False
+    else:
+        return True
 
 def isNumeric(str):
     try:
@@ -62,8 +96,16 @@ def isNumeric(str):
     except ValueError:
         return False
 
-while(True):
-    testinput = input()
-    if testinput == "login":
-        login()
+def ReadVSF(filename, validServices):
+    fstream = open(filename, "r")
+    for line in fstream:
+        service = line
+        validServices.append(service)
 
+def main():
+    while(True):
+        testinput = input()
+        if testinput == "login":
+            login()
+
+main()
